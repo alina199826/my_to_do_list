a65ac962a874f3f7d989f809710de59929929416
Alina <gartok.alina@gmail.com>
2022-11-21 01:11:06 +0600

    реализовала массовое удаление

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -11,6 +11,13 @@ class IndexView(TemplateView):
         context['tasks'] = Task.objects.all()
         return context
 
+    def post(self, request, *args, **kwargs):
+        for task_pk in request.POST.getlist('tasks'):
+            Task.objects.get(pk=task_pk).delete()
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
+
+
 
 
 class TaskView(TemplateView):
@@ -21,23 +28,7 @@ class TaskView(TemplateView):
         context['task'] = get_object_or_404(Task, pk=kwargs.get('pk'))
         return context
 
-# def create_task(request):
-#     if request.method == "GET":
-#         form = TaskForm()
-#         return render(request, "create.html", {'form': form})
-#     elif request.method == "POST":
-#         form = TaskForm(data=request.POST)
-#         if form.is_valid():
-#             new_task = Task.objects.create(
-#                 summary=form.cleaned_data['summary'],
-#                 description=form.cleaned_data['description'],
-#                 type=form.cleaned_data['type'],
-#                 status=form.cleaned_data['status'],
-#
-#             )
-#             return redirect('view', pk=new_task.pk)
-#         else:
-#             return render(request, "create.html", {'form': form})
+
 
 class CreateTask(TemplateView):
     template_name = 'create.html'
@@ -59,31 +50,6 @@ class CreateTask(TemplateView):
             return self.render_to_response(context)
 
 
-# def task_update(request, pk):
-#     task = get_object_or_404(Task, pk=pk)
-#     if request.method == 'GET':
-#         form = TaskForm(initial={
-#             'summary': task.summary,
-#             'description': task.description,
-#             'type': task.type,
-#             'status': task.status,
-#             'created_at': task.created_at,
-#             'updated_at': task.updated_at
-#         })
-#         return render(request, 'task_update.html', {'form': form})
-#     elif request.method == "POST":
-#         form = TaskForm(data=request.POST)
-#         if form.is_valid():
-#             task.summary = form.cleaned_data.get('summary')
-#             task.type = form.cleaned_data.get('type')
-#             task.description = form.cleaned_data.get('description')
-#             task.status = form.cleaned_data.get('status')
-#             task.created_at = form.cleaned_data.get('created_at')
-#             task.updated_at = form.cleaned_data.get('updated_at')
-#             task.save()
-#             return redirect('view', pk=task.pk)
-#         else:
-#             return render(request, 'task_update.html', {'form': form})
 
 class TaskUpdate(View):
 
@@ -110,14 +76,6 @@ class TaskUpdate(View):
         else:
             render(request, 'task_update.html', {'form': form, 'task': task})
 
-
-# def task_delete(request, pk):
-#     task = get_object_or_404(Task, pk=pk)
-#     if request.method == "GET":
-#         return render(request, 'task_delete.html', {'task': task})
-#     elif request.method == "POST":
-#         task.delete()
-#         return redirect('index')
 
 
 class TaskDelete(View):
