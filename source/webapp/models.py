a731de4c5847ae2c03f88ea02c61d6d965847abe
Alina <gartok.alina@gmail.com>
2022-11-25 18:47:24 +0600

    убрала лишний код

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -15,7 +15,7 @@ class Type(models.Model):
         return f'{self.pk}. {self.title}'
 
 class Task(models.Model):
-    summary = models.CharField(max_length=500, null=False, blank=False, verbose_name="summary")
+    summary = models.CharField(max_length=50, null=False, blank=False, verbose_name="summary")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
     description = models.TextField(max_length=2000, null=True, blank=True, verbose_name="description")
