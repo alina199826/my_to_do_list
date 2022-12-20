5b0ddca74b5c1d961cd6b54238ac65aff2d68e29
Alina <gartok.alina@gmail.com>
2022-12-20 13:33:13 +0600

    import logout

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.contrib.auth.forms import User
 
