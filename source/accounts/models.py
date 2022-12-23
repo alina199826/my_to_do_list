ddf19fb7e9811dcdd7d64f4893ba27e35286ce33
Alina <gartok.alina@gmail.com>
2022-12-23 23:48:59 +0600

    добавила модель profile, накатила миграции

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    biography = models.TextField(max_length=1000, null=True, blank=True, verbose_name='Дата Роджения')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_avatar', verbose_name='Фото')
--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from accounts.views import RegisterView
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'accounts'
 
@@ -11,4 +12,4 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', RegisterView.as_view(), name='create')
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
