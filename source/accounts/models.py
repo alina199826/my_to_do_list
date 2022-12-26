0eb38a5bd7b756728ee7e1fe5436513e380309b6
Alina <gartok.alina@gmail.com>
2022-12-26 23:42:30 +0600

    настроила админку

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -4,9 +4,14 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE,
                                 verbose_name='Пользователь')
-    biography = models.TextField(max_length=1000, null=True, blank=True, verbose_name='Дата Роджения')
+    biography = models.TextField(max_length=1000, null=True, blank=True, verbose_name='Биография')
     avatar = models.ImageField(null=True, blank=True, upload_to='user_avatar', verbose_name='Фото')
     link = models.URLField(max_length=200, null=True, blank=True, verbose_name='Cсылка')
+
+
+
+    def __str__(self):
+        return f"{self.user}"
 
 
     class Meta:
